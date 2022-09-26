c7357ca6fb0b042f238efbcd820e1331980f4485
giovaner10 <giovanerr10@gamil.com>
2022-09-26 15:22:22 -0300

    adicao dos cards, ajuste nas rotas, adicao do service para a leitura dos ususarios

--- src/app/home/menu/menu.page.ts
+++ src/app/home/menu/menu.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { HomeService } from '../home.service';
+import { Usuarios } from '../usuarios';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
-export class MenuPage implements OnInit {
+export class MenuPage  {
 
-  constructor() { }
+  usuarios = new Array<Usuarios>();
+  constructor(private service: HomeService, private alertController: AlertController, private rota: Router) {
+    this.service.getUsuarios().subscribe(response => (this.usuarios = response));
 
-  ngOnInit() {
   }
+  
+  
+;
+
 
 }
