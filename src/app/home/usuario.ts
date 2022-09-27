393e528f2a04fb51fed9eccbaf8f631ca400626c
giovaner10 <giovanerr10@gamil.com>
2022-09-26 22:13:57 -0300

    ajustes na class usuarios

--- src/app/home/usuario.ts
+++ src/app/home/usuario.ts
@@ -8,5 +8,17 @@ export class Usuarios{
   endereco: string;
   id: number;
 
+
+  constructor(login: string, senha: string, cpf: string, nome: string, foto: string, idade: string, endereco: string, id: number){
+    this.login = login;
+    this.senha = senha;
+    this.cpf = cpf;
+    this.nome = nome;
+    this.foto = foto;
+    this.idade = idade;
+    this.endereco = endereco;
+    this.id = id;
+  }
+
   
 }
